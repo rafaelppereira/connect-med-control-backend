b7e188fc0705a0a4f4ee715633872e2b1e35ca46
Rafael <rafaelsantospereira03@gmail.com>
2022-06-26 11:18:27 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8080;
 dotenv.config();
 const app = express();
   
-app.use(cors({}));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
